498f0d66174b95b6427559dbdee4980f95ed5dbb
Wei Han <noelng26@gmail.com>
2023-12-05 00:30:59 +0800

    updates

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,15 +3,15 @@ import Link from 'next/link';
 
 const Navbar = () => {
     return (
-        <nav className="bg-gray-800 p-1 mt-0 w-full fixed top-0 z-10">
+        <nav className="bg-gray-800 bg-opacity-50 p-1 mt-0 w-full fixed top-0 z-10">
             <ul className="flex justify-center space-x-4">
-                <li className="text-white ml-1">
+                <li className="text-xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
                     <Link href="/">Home</Link>
                 </li>
-                <li className="text-white">
+                <li className="text-xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
                     <Link href="/about">About</Link>
                 </li>
-                <li className="text-white">
+                <li className="text-xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
                     <Link href="/contact">Contact</Link>
                 </li>
             </ul>
