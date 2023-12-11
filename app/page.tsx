73a2423111ece7b1028f533d01a05148a7c815d2
Wei Han <noelng26@gmail.com>
2023-12-11 16:19:12 +0800

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,60 @@
+"use client"
+
+import React, { useState, useEffect } from 'react';
+import useTypewriter from 'react-typewriter-hook';
+import Lemon from './lemon.png';
+
+let id = 0;
+
 export default function Home() {
+  const [text, setText] = useState('Lemons');
+  const [lemons, setLemons] = useState([]);
+  const magicWriter = useTypewriter(text);
+
+  useEffect(() => {
+    if (magicWriter === 'Lemons') {
+      setTimeout(() => {
+        setText('');
+      }, 2000);
+    } else if (magicWriter === '') {
+      setTimeout(() => {
+        setText('Lemons');
+      }, 2000);
+    }
+  }, [magicWriter]);
+
+  const handleClick = () => {
+    const top = Math.random() * 100 + '%';
+    const left = Math.random() * 100 + '%';
+    const newLemon = { id: id++, top, left };
+    setLemons([...lemons, newLemon]);
+    setTimeout(() => {
+      setLemons(lemons => lemons.filter(lemon => lemon.id !== newLemon.id));
+    }, 5000);
+  }
+
   return (
     <main className="flex flex-col items-center justify-center p-2">
+      <div className="flex flex-col min-h-screen items-center justify-center relative ">
+        <p className="text-4xl md:text-8xl font-bold text-center text-yellow-400 absolute z-10">
+          {magicWriter}
+          <span className="text-yellow-400 animate-blink">|</span>
+        </p>
+        <p className="text-4xl md:text-8xl font-bold text-center text-yellow-600 absolute z-[-1] mt-1.5 mr-1.5">
+          {magicWriter}
+          <span className="text-yellow-600 animate-blink">|</span>
+        </p>
+        <p className="text-4xl md:text-8xl font-bold text-center text-yellow-800 absolute z-[-2] mt-3 mr-3">
+          {magicWriter}
+          <span className="text-yellow-800 animate-blink">|</span>
+        </p>
 
-      <div className="flex min-h-screen items-center justify-center relative ">
-        <p className="text-8xl font-bold text-center text-yellow-400 absolute z-10">Lemons</p>
-        <p className="text-8xl font-bold text-center text-yellow-600 absolute z-[-1] mt-1.5 mr-1.5">Lemons</p>
-        <p className="text-8xl font-bold text-center text-yellow-800 absolute z-[-2] mt-3 mr-3">Lemons</p>
+        <div className="h-60"></div>
+        <button onClick={handleClick} className="text-2xl md:text-4xl font-bold text-white text-center">Click me</button>
+        {lemons.map(lemon => (
+          <img key={lemon.id} src="https://i.ibb.co/6yQnjhZ/lemon.png" alt="Lemon" style={{ position: 'absolute', top: lemon.top, left: lemon.left, animation: 'spin 4s linear infinite' }} />
+        ))}
       </div>
-
     </main>
   )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,15 +3,15 @@ import Link from 'next/link';
 
 const Navbar = () => {
     return (
-        <nav className="bg-gray-800 bg-opacity-50 p-1 mt-0 w-full fixed top-0 z-10">
-            <ul className="flex justify-center space-x-4">
-                <li className="text-xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
+        <nav className="bg-gray-800 bg-opacity-10 p-4 mt-0 w-full fixed top-0 z-10">
+            <ul className="flex justify-center space-x-8">
+                <li className="text-xl bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 to-yellow-400 hover:text-yellow-300 transition duration-300">
                     <Link href="/">Home</Link>
                 </li>
-                <li className="text-xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
+                <li className="text-xl bg-clip-text text-transparent bg-gradient-to-r from-yellow-400 to-yellow-400 hover:text-yellow-300 transition duration-300">
                     <Link href="/about">About</Link>
                 </li>
-                <li className="text-xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
+                <li className="text-xl bg-clip-text text-transparent bg-gradient-to-r from-yellow-400 to-yellow-500 hover:text-yellow-300 transition duration-300">
                     <Link href="/contact">Contact</Link>
                 </li>
             </ul>
